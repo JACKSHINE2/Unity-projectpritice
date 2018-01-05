c11c1d50b147d43cb309ebbb95ef8a5fc0955023
Jack <33677306+JACKSHINE2@users.noreply.github.com>
2018-01-05 19:41:24 +0800

    SolarSystem
    planet rotation

---
+++ SolarSystem/Revolution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Revolution : MonoBehaviour {
+    //[HideInInspector]   //只会影响一句
+    public Transform traget;
+    //public GameObject star;
+    public float anglespeed;
+    Vector3 lastposition;
+    // Use this for initialization
+    void Start () {
+        lastposition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.RotateAround(traget.position,traget.up,anglespeed*Time.deltaTime);
+        Debug.DrawLine(transform.position, lastposition, Color.yellow);
+        lastposition = transform.position;
+
+    }
+}
---
+++ SolarSystem/angleSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angleSpeed : MonoBehaviour {
+    public float anglespeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * anglespeed * Time.deltaTime);
+
+	}
+}
