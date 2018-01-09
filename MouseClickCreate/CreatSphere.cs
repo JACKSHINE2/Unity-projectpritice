79837bfb6a2ae0cba4c93ebedd30e57ae855680f
Jack <33677306+JACKSHINE2@users.noreply.github.com>
2018-01-09 17:12:46 +0800

    Mouse
    the transform between World and Screen.

---
+++ MouseClickCreate/CreatSphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatSphere : MonoBehaviour {
+    public GameObject sphere;
+    float z;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetMouseButtonDown(0)) {
+            z = Camera.main.WorldToScreenPoint(sphere.transform.position).z;
+            Vector3 pos1 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, z);
+            Vector3 pos2 = Camera.main.ScreenToWorldPoint(pos1);
+            GameObject obj = Instantiate(sphere, pos2, Quaternion.identity);
+            Destroy(obj,3);
+        }
+     }
+}
---
+++ MouseClickCreate/MouseCursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Cursor is invisible
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //press the "ESC"
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+        }
+	}
+}
