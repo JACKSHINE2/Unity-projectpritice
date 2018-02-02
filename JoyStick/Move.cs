58f6b24be016624ee76c0faf56041c078230d6ec
Jack <33677306+JACKSHINE2@users.noreply.github.com>
2018-02-02 16:31:29 +0800

    JoyStick

---
+++ JoyStick/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    RectTransform dir;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        dir = GameObject.Find("content").GetComponent<RectTransform>();
+	}
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(dir.anchoredPosition.x, 0, dir.anchoredPosition.y) * Time.fixedDeltaTime * speed;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ JoyStick/Restrict.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Restrict : MonoBehaviour {
+    RectTransform rectTransform;
+    public RectTransform parent;
+    // Use this for initialization
+    void Start () {
+        rectTransform = GetComponent<RectTransform>();
+        //rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //print(rectTransform.rect.ToString());
+        //print(rectTransform.anchoredPosition.ToString());
+        //print(rectTransform.sizeDelta.ToString());
+        //print(rectTransform.rect.ToString());
+        //print(rectTransform.offsetMin);        
+        if (rectTransform.anchoredPosition.magnitude>Mathf.Abs(rectTransform.rect.x-parent.rect.x))
+        {
+            rectTransform.anchoredPosition = rectTransform.anchoredPosition.normalized * Mathf.Abs(rectTransform.rect.x - parent.rect.x);
+        }
+
+    }
+}
