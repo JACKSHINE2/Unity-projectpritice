dd8b80e90b020236e35cfe720592223049f62028
Jack <33677306+JACKSHINE2@users.noreply.github.com>
2018-02-02 16:33:47 +0800

    rubbish

---
+++ OpenDoor/AnimControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour {
+    Animator animator;
+    public Transform rightHand;
+    public Transform door;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        door = GameObject.FindWithTag("Finish").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float ver = Input.GetAxis("Vertical");
+        animator.SetBool("Walk", ver != 0);
+        
+	}
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (Vector3.Distance(rightHand.position, transform.position) < 1.6f)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, rightHand.rotation);
+            if (door.eulerAngles.y < 24)
+            {
+                door.RotateAround(new Vector3(0, 1, -0.8f), transform.up, 0.5f);
+            }
+        }
+    }
+}
---
+++ WeaponTool.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class WeaponTool : EventTrigger {
+    Transform oriPosition;
+    Transform bagParent;
+	// Use this for initialization
+	void Start () {
+        bagParent = GameObject.Find("BagParent").transform;
+
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        oriPosition = transform.parent;
+        SetParentAndPos(transform, bagParent);
+        //关闭当前ui的射线检测
+        GetComponent<Image>().raycastTarget = false;
+
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;        
+
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        Transform item = eventData.pointerCurrentRaycast.gameObject.transform;
+        //if (item.gameObject == null) return;
+
+        if (item.gameObject.tag=="bag")
+        {
+            transform.parent = item;
+
+            transform.position = item.position;
+            //SetParentAndPos(transform, item);
+
+        }
+        else if (item.gameObject.tag=="good")
+        {
+            transform.parent = item.parent;
+            transform.position = item.position;
+
+            item.parent = oriPosition;
+            item.position = oriPosition.position;
+
+            //SetParentAndPos(transform, item.parent);
+            //SetParentAndPos(item, oriPosition);
+        }
+        else
+        {
+            transform.parent = oriPosition;
+            transform.position = oriPosition.position;
+
+            //SetParentAndPos(transform, beginP);
+
+        }
+        GetComponent<Image>().raycastTarget = true;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void SetParentAndPos(Transform item ,Transform mparent)
+    {
+        item.position = mparent.position;
+        item.parent = mparent;
+    }
+
+   
+}
